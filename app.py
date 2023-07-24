100a954997bd4eaa1722549ae6dad9b5ecdc27bc
Tejaswini Uma Sudhir <119094222+tejucodes10@users.noreply.github.com>
2023-07-24 17:07:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------
+# Imports
+from flask import Flask, render_template
+
+# ------------------------------------------------------------------------------
+# Flask App
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
